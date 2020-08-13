99d2e49d2b57358b479ceba7593cd1ffe977680f
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-08-13 20:06:04 -0300

    final

--- horizontal_tip.py
+++ horizontal_tip.py
@@ -6,8 +6,8 @@ def triangulo_lateral(largura):
     n = 2*largura-1
     for n in range(1,largura +1):
         print('*'*n)
-        if n == 4:
+        if n == largura:
            for n in range(largura -1, 0, -1):
                 print('*'*n)
        
-triangulo_lateral(4)
+triangulo_lateral(10)
