e60ffebed9d73f353351a7b4677e68f66059a1dd
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-08-15 09:21:06 -0300

    done

---
+++ funcao_quadratic.py
@@ -0,0 +1,10 @@
+
+def gerador_funcao_quadratica(a, b, c):
+    """retorna f(x)= a*x**2 + b*x +c"""
+    return lambda x: a * x ** 2 + b * x + c ## o lambda x sera o teste do print
+
+teste = gerador_funcao_quadratica(2, 3, -5)##gera do "f(x)"
+
+print(teste(0))
+print(teste(1))
+print(teste(2))
