fd2caaa0f9c4a5fc3d9a8d92f9e04997467e9e6c
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-08-17 17:35:14 -0300

    tweets

--- filter.py
+++ filter.py
@@ -8,7 +8,18 @@ media = statistics.mean(valores)
 maior_q_media = filter(lambda x: x > media, valores)## filter(funcao, iteravel)
 print(list(maior_q_media))
 
-""" eli,imando valores ausentes"""
+""" elimimando valores ausentes"""
 lista = ["um","", "dois", "tres", "quatro", "seis", "", ""]
 res = filter(None, lista)
 print(list(res))
+
+"""Filtrar valore sinativos de dicionarios dentro de uma lista"""
+
+dados = [{"username" : "calos", "tweets": ["oi", "tudo bem"]},
+         {"username" : "alberto", "tweets": ["oi"]},
+         {"username" : "sonia", "tweets": ["tudo bem"]},
+         {"username" : "luffy", "tweets": []},
+         {"username" : "fauno", "tweets": []}]
+
+inativos = list(filter(lambda usuario: len(usuario["tweets"])==0,dados))
+print(inativos)
