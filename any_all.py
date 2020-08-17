0314516a97103654df37d29e1f1751a83333f647
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-08-17 20:54:57 -0300

    any

--- any_all.py
+++ any_all.py
@@ -1,7 +1,11 @@
-# all() return True if all variables are true or if the data it is empty (in boolean 0 is false)
+# all() return True if all iterables are true or if the data it is empty (in boolean 0 is false)
 
 print(all([1,2,3,4,5]))
 print(all([0,1,2,3,4,5]))
 print(all("oi"))
 print(all([]))
 
+# any() return Trye if any of the iterables are True
+
+print(any([0,1,2,3,4]))
+print(any([0]))
