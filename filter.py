a6e304c9d439c146c520e9d7d6596d6f024a7020
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-08-17 16:53:26 -0300

    filter with none

--- filter.py
+++ filter.py
@@ -2,8 +2,13 @@
 
 import statistics
 
-valores = 1,2,3,4,5,6
+valores = [1,2,3,4,5,6]
 
 media = statistics.mean(valores)
-maior_q_media = filter(lambda x: x > media, valores)
+maior_q_media = filter(lambda x: x > media, valores)## filter(funcao, iteravel)
 print(list(maior_q_media))
+
+""" eli,imando valores ausentes"""
+lista = ["um","", "dois", "tres", "quatro", "seis", "", ""]
+res = filter(None, lista)
+print(list(res))
