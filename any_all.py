a6c7572cb5d494bfe4230a6e7447819e842b17e4
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-08-17 20:53:08 -0300

    all

---
+++ any_all.py
@@ -0,0 +1,7 @@
+# all() return True if all variables are true or if the data it is empty (in boolean 0 is false)
+
+print(all([1,2,3,4,5]))
+print(all([0,1,2,3,4,5]))
+print(all("oi"))
+print(all([]))
+
