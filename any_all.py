c7c6ab5adac7304ae36ae913a60c6f36e4d0e868
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-08-17 21:20:06 -0300

    update

--- any_all.py
+++ any_all.py
@@ -5,7 +5,22 @@ print(all([0,1,2,3,4,5]))
 print(all("oi"))
 print(all([]))
 
+nomes = ["Carlos", "Camile", "Cado", "Caio"]
+print(all([nome[0] == 'C' for nome in nomes]))
+print(all([nome[1] =='v' for nome in nomes]))
+
+print(all([num%2 ==0 for num in [2,4,6,8,10]]))
+#print(all([num for num in [2,4,6,8] if num % 2 != 0]))  tem alguma coisa errada
+print(all([num%2 !=0 for num in [2,4,6,8,10]]))
+
+
+
+
+
+
+
+
 # any() return Trye if any of the iterables are True
 
-print(any([0,1,2,3,4]))
-print(any([0]))
+#print(any([0,1,2,3,4]))
+#print(any([0]))
