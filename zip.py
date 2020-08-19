476903b70f09004951b6f9ad0ba217c2a0012478
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-08-19 17:29:26 -0300

    notas

---
+++ zip.py
@@ -0,0 +1,8 @@
+"""gerar um lista com a maior nota de cada aluno usando zip"""
+
+prova1 = (8, 7, 3)
+prova2 = (3, 5, 7)
+aluno = ("Maria", "Pedro", "Jose")
+
+maior_nota = {dado[0]: max(dado[1], dado[2]) for dado in zip(aluno, prova1, prova2)}
+print(maior_nota)
