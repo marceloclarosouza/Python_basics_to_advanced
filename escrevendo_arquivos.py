7034124d00c9bc4d96197e1d6d051efc3fdd237a
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-08-27 17:08:10 -0300

    final

--- escrevendo_arquivos.py
+++ escrevendo_arquivos.py
@@ -1,4 +1,16 @@
-with open("texto.txt", "w") as data:##criar um arquivo
+"""with open("texto.txt", "w") as data:##criar um arquivo
     ##digite a sequencia de instrucoes
     data.write("Escreva o conteuúdo de ...\n")
-    data.write("Conitnue escrevendo...\n")
+    data.write("Conitnue escrevendo...\n")"""
+    
+    
+"""Exemplo usando loop"""
+
+with (open ("fruta.txt", "w")) as data:
+    while True:
+        fruta = input("Digite o nome de uma fruta ou sair\n")
+        if fruta != 'sair':
+            data.write(fruta)
+            data.write("\n")
+        else:
+            break
