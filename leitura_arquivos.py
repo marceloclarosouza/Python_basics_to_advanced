91f6f92ed3091e25b108ec01747e00f354461a3e
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-08-27 11:53:51 -0300

    seek

--- leitura_arquivos.py
+++ leitura_arquivos.py
@@ -2,3 +2,4 @@ data = open("texto.txt", "r")##abrir arquivo modo leitura
 data = open("texto.txt", "w")##abrir arquivo nodo escrita
 
 data.read()##ler o arquivo
+data.seek(0)##move o cursor para o ínício do arquivo
