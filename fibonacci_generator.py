c44542f7c1da532aa6ffe07b55fa6f6dff21f3bc
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-09-01 20:42:17 -0300

    final

--- fibonacci_generator.py
+++ fibonacci_generator.py
@@ -15,18 +15,18 @@ print("\n")
 print(fibo_pythonic(7))#print a LIST
 print("\n\n")
 
-#fibonacci com generator
+#fibonacci com generator consome 150vezes menos memória !!!
 
 def fibo_generator(max):
-    nums = []
-    a,b = 0,1
-    while len(nums)<max:
-        nums.append(b)
-        yield nums
+    a,b, contador = 0,1,0
+    contador = 1
+    while contador<max:
+        yield b
         a,b =b, a+b
+        contador += 1
         
 for n in fibo_generator(7):
     print (n)
 print("\n")
-print(fibo_generator(7))##generator list
+print(fibo_generator(7))##generator
 print(list(fibo_generator(7)))##convertendo o generator em LIST
