529d28abe6a1c0fbcd876c1cba72a8c6362945ba
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-09-01 20:24:02 -0300

    generator

--- fibonacci_generator.py
+++ fibonacci_generator.py
@@ -13,5 +13,20 @@ for n in fibo_pythonic(7):#print a number per line
     
 print("\n")
 print(fibo_pythonic(7))#print a LIST
+print("\n\n")
 
 #fibonacci com generator
+
+def fibo_generator(max):
+    nums = []
+    a,b = 0,1
+    while len(nums)<max:
+        nums.append(b)
+        yield nums
+        a,b =b, a+b
+        
+for n in fibo_generator(7):
+    print (n)
+print("\n")
+print(fibo_generator(7))##generator list
+print(list(fibo_generator(7)))##convertendo o generator em LIST
