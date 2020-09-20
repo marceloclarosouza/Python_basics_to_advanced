8b7394ce7a4a9cf828b20be61503d860ae72eb3d
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-09-20 09:15:34 -0300

    update

--- lambda.py
+++ lambda.py
@@ -1,7 +1,7 @@
 """
 funções lambdas são funções sem nome
 """
-nome = ['aline simsic', 'pud d. luffy', 'marcelo souza', 'panceta gorda', 'jimbei']
+nome = ['aline simsic', 'pug d. luffy', 'marcelo souza', 'panceta gorda', 'jimbei']
 
 nome.sort(key=lambda sobrenome: sobrenome.split(' ')[-1].lower())##ordenar os nomes pelo último sobrenome
 print(nome)
